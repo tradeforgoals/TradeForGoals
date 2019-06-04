869939081279b9e022b96ccfe477f40e3e5c08f0
Marc <marcveens@hotmail.com>
2019-06-04 17:10:25 +0200

    Made sure an empty JSON response is returned

--- tradeforgoals/src/main/java/com/hva/tradeforgoals/api/customerEndpoint.java
+++ tradeforgoals/src/main/java/com/hva/tradeforgoals/api/customerEndpoint.java
@@ -1,6 +1,7 @@
 package com.hva.tradeforgoals.api;
 
 import com.hva.tradeforgoals.domain.Customer;
+import com.hva.tradeforgoals.domain.EmptyResponse;
 import com.hva.tradeforgoals.persistence.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -31,6 +32,10 @@ public class customerEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response findCustomer(@PathParam("id") String id) {
         Customer customer = customerService.findById(id);
+        
+        if (customer == null) {
+            return Response.ok(new EmptyResponse()).build();
+        }
         return Response.ok(customer).build();
     }
     
