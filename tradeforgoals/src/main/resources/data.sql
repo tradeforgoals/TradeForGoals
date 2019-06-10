6d6feba3ee8e07e643f5b0fb2bb45b266779e579
simon <sjvanzwoll@gmail.com>
2019-06-10 16:12:53 +0200

    revert

--- tradeforgoals/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customer (customer_id,first_name) VALUES ('1','Jorik');
-INSERT INTO customer (customer_id,first_name) VALUES ('2','Marc');
-INSERT INTO customer (customer_id,first_name) VALUES ('3','Simon');
-INSERT INTO customer (customer_id,first_name) VALUES ('4','Thomas');
