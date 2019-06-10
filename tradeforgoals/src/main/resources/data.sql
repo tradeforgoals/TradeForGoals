d94bbb4dcead085a44d547352c9ab6587a05eed8
simon <sjvanzwoll@gmail.com>
2019-06-10 15:27:38 +0200

    test prefill db on start-up

---
+++ tradeforgoals/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (customer_id,first_name) VALUES ('1','Jorik');
+INSERT INTO customer (customer_id,first_name) VALUES ('2','Marc');
+INSERT INTO customer (customer_id,first_name) VALUES ('3','Simon');
+INSERT INTO customer (customer_id,first_name) VALUES ('4','Thomas');
